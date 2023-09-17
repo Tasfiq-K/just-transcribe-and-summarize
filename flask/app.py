05b4e10969c6a504c71d3d76dfb8693e501bad6b
tasfiq-k <tasfiq.kamran@gmail.com>
2023-09-17 17:03:20 +0600

    added flask and templates dirs

---
+++ flask/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    input_text = ""
+    
+
+    if request.method == 'POST':
+        input_text = request.form.get('input_text', '')
+        print(input_text)    
+        output = predict_tasks(input_text)[0]
+        confidence_list = output['confidences']
+        tasks = [conf['label'] for conf in confidence_list if conf['confidence'] >= 0.3]
+        task_text = ""
+
+        # for idx, task in enumerate(tasks):
+        #     task_text = task_text + task
+        #     if idx != len(tasks)-1:
+        #         task_text = task_text + ", "
+
+        task_text = ", ".join(tasks)
+
+        print(task_text)
+        return render_template("index.html", input_text=input_text, output_text=task_text)
+    else:
+        return render_template("index.html")
+
+def predict_tasks(input_text):
+    response = requests.post("https://g0blas-paper-task-suggestion.hf.space/run/predict",
+                             json={
+                                 "data": [
+                                     input_text
+                                ]
+                             }).json()
+    data = response["data"]
+    
+    return data
+
+if __name__ == "__main__":
+    app.run(debug=True)
