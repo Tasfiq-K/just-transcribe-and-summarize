5bfb9c444d24fc20951606ab4260eec6d1287090
Tasfiq Kamran <52997915+Tasfiq-K@users.noreply.github.com>
2023-09-19 23:06:28 +0000

    edited app.py and functions.py

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,21 @@
+import os
 import requests
+from functions import predict_tags, audio_to_text, summarizer, keyword_extractor, media_to_audio
 from flask import Flask, render_template, request
-from functions import predict_tags
+
 
 app = Flask(__name__)
 
+# UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/')
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 @app.route("/", methods=['GET', 'POST']) # call the home function when submitting the form to the current route
 def home():
-    
+
     input_text = ""
-    
+    save_loc = "static"
+
 
     if request.method == 'POST':
         input_text = request.form.get('input_text', '')
@@ -16,22 +23,60 @@ def home():
         media_file = request.files['input_media']
         print(input_text)
 
-        classifications = predict_tags(input_text)[0]
-        confidence_list = classifications['confidences']
-        tags = [conf['label'] for conf in confidence_list if conf['confidence'] >= 0.3]
-        tags_text = ""
+        if input_link:
+            transcription = audio_to_text(url=input_link, file_path=save_loc)
+            summary = summarizer(transcription)
+            keywords = keyword_extractor(transcription)
+            classifications = predict_tags(transcription)[0]
+            confidence_list = classifications['confidences']
+            tags = [conf['label'] for conf in confidence_list if conf['confidence'] >= 0.3]
+            tags_text = ""
+            tags_text = ", ".join(tags)
 
-        # for idx, task in enumerate(tasks):
-        #     task_text = task_text + task
-        #     if idx != len(tasks)-1:
-        #         task_text = task_text + ", "
+            print(tags_text)
+            return render_template("results.html", input_text=input_text, output_text=tags_text)
 
-        tag_text = ", ".join(tags)
+        if media_file:
+            # check and save the media file
+            if media_file.filename != "":
+                fn = media_file.filename
+                if not os.path.exists(save_loc):
+                    os.mkdir('static')
+                    media_file.save(os.path.join(save_loc, fn))
+                else:
+                    media_file.save(os.path.join(save_loc, fn))
+                
+                # convert media to text
+                transcription = audio_to_text(audio_file=media_to_audio(fn, save_loc), file_path=save_loc)
+                
+                summary = summarizer(transcription) # get the summary
+                keywords = keyword_extractor(transcription) # get the keywords
+                
+                # classification stuff
+                classifications = predict_tags(transcription)[0]    
+                confidence_list = classifications['confidences']
+                tags = [conf['label'] for conf in confidence_list if conf['confidence'] >= 0.3]
+                tags_text = ""
+                tags_text = ", ".join(tags)
+                
+            return render_template("results.html", input_text=input_text, output_text=tags_text)
 
-        print(tag_text)
-        return render_template("results.html", input_text=input_text, output_text=tag_text)
-    else:
-        return render_template("results.html")
+            
+            # else:
+        
+        if input_text:
+            transcription = input_text
+            summary = summarizer(transcription)
+            keywords = keyword_extractor(transcription)
+            classifications = predict_tags(transcription)[0]
+            confidence_list = classifications['confidences']
+            tags = [ conf['label'] for conf in confidence_list if conf['confidence'] >= 0.3]
+            tags_text = ""
+            tags_text = ", ".join(tags)
+
+        return render_template("resutls.html", input_text=input_text, output_text=tags_text)
+
+    return render_template("home.html")
 
 
 
--- flask/functions.py
+++ flask/functions.py
@@ -1,73 +1,150 @@
 import os
 import requests
-from flask import Flask, render_template, request
+# from flask import Flask, render_template, request
 import torch
 import whisperx
 from transformers import pipeline
 from pytube import YouTube
 from pytube.exceptions import AgeRestrictedError
+from keybert import KeyBERT
+from moviepy.editor import VideoFileClip
+
 
 def predict_tags(input_text):
-    response = requests.post("https://g0blas-paper-task-suggestion.hf.space/run/predict",
-                             json={
-                                 "data": [
-                                     input_text
-                                ]
-                             }).json()
+    """
+    Predicts the tags for this blog/text
+    args:
+        input_text: [str] Text data
+    returns: tags with their probability score
+    """
+    response = requests.post("https://g0blas-blog_tags_classifier.hf.space/run/predict", json={"data": [input_text] }).json()
     data = response["data"]
-    
+
     return data
 
-def audio_to_text(url: str):
+
+def audio_to_text(url=None, audio_file=None, file_path=None):
     """
     Args:
         link: [str] -> takes a youtube video link
-    returns:
+    returns: transcribed text from video 
+
     """
-
-
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     bs = 16
     compute_type = "float16"
-    
-    f_name = 'audio_file.mp4' # the file name to save with
-    yt = YouTube(url)
+    model = whisperx.load_model("large-v2", device, compute_type=compute_type)
 
-    # if GPU available run this block
-    if device == 'cuda': 
-        try:
-            model = whisperx.load_model("large-v2", device, compute_type=compute_type)
-            yt.streams.filter(only_audio=True)
-            stream = yt.streams.get_by_itag(139)
-            stream.download('', f_name)
-            audio = whisperx.load_audio(f_name)
+    if url:
+
+        f_name = 'audio_file.mp4' # the file name to save with
+        yt = YouTube(url)
+
+        # if GPU available run this block
+        if device == 'cuda':
+            try:
+                yt.streams.filter(only_audio=True)
+                stream = yt.streams.get_by_itag(139)
+                stream.download(os.path.join(file_path, f_name))
+                audio = whisperx.load_audio(os.path.join(file_path, f_name))
+                result = model.transcribe(audio, batch_size=bs, language='en')
+                res = [ i['text'] for i in result['segments'] ]
+                # os.remove(f"static/{f_name}")
+
+                return res[0]
+
+            except AgeRestrictedError:
+                res = ""
+                return res
+
+        else:   # else run this block
+
+            try:
+                yt.streams.filter(only_audio=True)
+                stream = yt.streams.get_by_itag(139)
+                stream.download(os.path.join(file_path, f_name))
+                audio = whisperx.load_audio(os.path.join(file_path, f_name))
+                result = model.transcribe(audio, language='en')
+                res = [ i['text'] for i in result['segments'] ]
+                # os.remove(f"static/{f_name}")
+
+                return res[0]
+
+            except AgeRestrictedError:
+
+                res = ""
+                return res
+
+    elif audio_file:
+        # so, It's already in the audio format
+        if device == 'cuda':
+            audio = whisperx.load_audio(os.path.join(file_path, audio_file)) # load the audio file
             result = model.transcribe(audio, batch_size=bs, language='en')
             res = [ i['text'] for i in result['segments'] ]
-            os.remove(f"{f_name}")
-            
+            # os.remove(os.path.join())
             return res[0]
-        
-        except AgeRestrictedError:
-            res = ""
-            return res
-    
-    else:   # else run this block
-        
-        try:
-            model = whisperx.load_model("large-v2", device, compute_type=compute_type)
-            yt.streams.filter(only_audio=True)
-            stream = yt.streams.get_by_itag(139)
-            stream.download('', f_name)
-            audio = whisperx.load_audio(f_name)
-            result = model.transcribe(audio, language='en')
-            res = [ i['text'] for i in result['segments'] ]
-            os.remove(f"{f_name}")
-            
-            return res[0]
-        
-        except AgeRestrictedError:
-            
-            res = ""
-            return res
 
+        audio = whisperx.load_audio(os.path.join(file_path, audio_file)) # load the audio file
+        result = model.transcribe(audio, language='en')
+        res = [ i['text'] for i in result['segments'] ]
+
+    return res[0]
+
+
+def summarizer(text):
+    """
+    A text summaraizer
+    args: 
+        text: text data
+    returns: returns the summarized version of the given text
+    """
+    max_length = 100
+    min_length = 20
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    summ_text = pipeline("summarization", model="pszemraj/led-large-book-summary", device=device)
+
+    summary = summ_text(text, min_length=min_length, max_length=max_length)
+
+    return summary[0][ 'summary_text' ]
+
+
+def keyword_extractor(text):
+    """
+    Extract keywords from the given text
+    args: 
+        text: Text data
+    returns: String of top 5 keywords
+    """
+    top_kw = ""
+    kw_model = KeyBERT()
+    keywords = kw_model.extract_keywords(text, top_n=5)
+    kws = [ i[0].title() for i in keywords ]
+    top_kw = ", ".join(kws)
+
+    return top_kw
+
+
+def media_to_audio(media_file, file_path, output_ext="mp4", output_file="audio"):
+    """
+    converts media files like MP4, MOV, WMV, FLV, AVI, AVCHD, WebM, MKV into an MP4 audio file
+    args: 
+        media_file: [str]: name of the media file
+        output_ext: outputs extension for the media file
+    """
+    allowed_ext = ['.mp4', ".mov", ".wmv", ".flv", ".avi", ".avchd", ".webm", ".mkv"]
+    output_fn = f"{output_file}.{output_ext}"
+    _, ext = os.path.splitext(os.path.join(file_path, media_file))
+
+    if ext.lower() not in allowed_ext:
+
+        ext = ", ".join(allowed_ext)
+        print(f"Unknown format! try using from one of these formats{ext}")
+
+    else:
+        # do the processing
+        clip = VideoFileClip(os.path.join(file_path, media_file))
+        clip.audio.write_audiofile(os.path.join(file_path, output_fn))
+
+    return output_fn
